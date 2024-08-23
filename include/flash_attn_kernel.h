da0f6c3d6601874a1cedbbcd93f453c5eb690bab
Wei-Yu <jcab1688@gmail.com>
2024-08-23 08:51:20 +0000

    format flash_attn_kernel.h

--- include/flash_attn_kernel.h
+++ include/flash_attn_kernel.h
@@ -3,7 +3,9 @@
 
 #include <torch/extension.h>
 
-void launch_forward_kernel(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor l, torch::Tensor m, torch::Tensor O);
-void launch_backward_kernel(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor O, torch::Tensor dQ, torch::Tensor dK, torch::Tensor dV, torch::Tensor dO, torch::Tensor l, torch::Tensor m); 
+void launch_forward_kernel(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor l, torch::Tensor m,
+                           torch::Tensor O);
+void launch_backward_kernel(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor O, torch::Tensor dQ,
+                            torch::Tensor dK, torch::Tensor dV, torch::Tensor dO, torch::Tensor l, torch::Tensor m);
 
-#endif 
+#endif
